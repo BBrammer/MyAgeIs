b2378b8aabac6883f31d7938e9321e38cbc43659
BBrammer <brandon.brammer@patelhs.org>
2020-09-24 14:08:30 +0000

    I made an if else statement about if my birthday was today I would add one to my age

--- Main.java
+++ Main.java
@@ -1,5 +1,14 @@
 class Main {
   public static void main(String[] args) {
-    System.out.println("Hello world!");
+    // Fill in values for these variables
+    int myAge = 15;
+    int myBirthday = 511
+    int todaysDate = 922
+    if(myBirthday==todaysDate){
+    add one to myAge
+    }else{
+    don't add one to myAge
+    }
+    System.out.println("I am " + myAge + " years old.");
   }
 }
